937fa1ec50e530cbff5ba12f0443b3ad3adeec0d
Nastya <nastya.syasyafi@gmail.com>
2024-08-28 16:16:05 +0300

    task 1-2-3

---
+++ src/components/App/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import "./App.module.css";
+import Description from "../Description/Description";
+import Feedback from "../Feedback/Feedback";
+import Options from "../Options/Options";
+import Notification from "../Notification/Notification";
+
+const getInitialData = () => {
+  const savedData = window.localStorage.getItem("my-data");
+  // console.log(savedData)
+  return savedData !== null
+    ? JSON.parse(savedData)
+    : { good: 0, neutral: 0, bad: 0 };
+};
+
+export default function App() {
+  const [data, setData] = useState(getInitialData);
+
+  useEffect(() => {
+    window.localStorage.setItem("my-data", JSON.stringify(data));
+  }, [data]);
+
+  const resetFeedback = () => {
+    setData({ good: 0, neutral: 0, bad: 0 });
+  };
+  const updateFeedback = (feedbackType) => {
+    setData({ ...data, [feedbackType]: data[feedbackType] + 1 });
+  };
+  const totalFeedback = data.good + data.neutral + data.bad;
+  const percentOfGood = Math.round((data.good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        reset={resetFeedback}
+        total={totalFeedback}
+      />
+
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={data.good}
+          neutral={data.neutral}
+          bad={data.bad}
+          total={totalFeedback}
+          percent={percentOfGood}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,11 @@
+export default function Options({ updateFeedback, reset, total }) {
+  const shouldShowReset = total >= 1;
+  return (
+    <>
+      <button onClick={() => updateFeedback("good")}>Good</button>
+      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+      <button onClick={() => updateFeedback("bad")}>Bad</button>
+      {shouldShowReset && <button onClick={reset}>Reset</button>}
+    </>
+  );
+}
